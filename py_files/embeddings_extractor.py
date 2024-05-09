625dbe508d18932fc867e23c009ee229496c800d
João Vedor <fc56311@alunos.fc.ul.pt>
2024-05-10 00:04:04 +0100

    Added the creating embeddings method

---
+++ py_files/embeddings_extractor.py
@@ -0,0 +1,69 @@
+import os
+import torch
+import numpy as np
+
+from owlready2 import *
+from transformers import BertModel, BertTokenizer
+from datetime import datetime
+
+
+def get_list_of_labels_from_ontology(owl_path):
+    onto = get_ontology("file://" + owl_path).load()
+    list_of_labels = []
+
+    # Has alternate labels ?
+    for obj in onto.classes():
+        if hasattr(obj, "label"):
+            labels = obj.label
+            for label in labels:
+                list_of_labels.append(label)
+
+    return list_of_labels
+
+
+def get_model_for_embedding(model_name):
+    return BertModel.from_pretrained(model_name)
+
+
+def get_tokenizer_for_embedding(model_name):
+    return BertTokenizer.from_pretrained(model_name)
+
+
+def get_embedding_from_owl_file(owl_path):
+    start_time = datetime.now()
+
+    print("Started To List The Labels")
+
+    list_of_labels = get_list_of_labels_from_ontology(owl_path)
+
+    print("Started To Get The Model And Tokenizer")
+
+    model_name = "bert-base-uncased"
+    model = get_model_for_embedding(model_name)
+    tokenizer = get_tokenizer_for_embedding(model_name)
+
+    print("Started To Tokenize The Labels")
+
+    tokenized_labels = [tokenizer(lbl, return_tensors="pt") for lbl in list_of_labels]
+
+    print("Start The Embedding")
+
+    with torch.no_grad():
+        embeddings = [model(**lbl)["last_hidden_state"].squeeze(0) for lbl in tokenized_labels]
+
+    averaged_embeddings = [torch.mean(embedding, dim=0) for embedding in embeddings]
+
+    finish_time = datetime.now()
+
+    print("Embedding Ended in: " + str((finish_time - start_time).total_seconds()))
+
+    return averaged_embeddings
+
+
+def get_embedding_file(owl_path, npy_file_name):
+
+    np.save(npy_file_name, get_embedding_from_owl_file(owl_path))
+
+
+get_embedding_file("anatomy-dataset/anatomy-dataset/human.owl", "human_embeddings.npy")
+get_embedding_file("anatomy-dataset/anatomy-dataset/mouse.owl", "mouse_embeddings.npy")
